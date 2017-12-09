152d39b5a2027314f625f0281dc2f0163bf24eee
Antonio Quintero <man.quintero@gmail.com>
2017-12-09 17:40:35 -0600

    Add pthread lib

--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <config.h>
+#include <pthread.h>
+
+static void * print_it(void * data)
+{
+	printf("Hello from %s!\n", (const char *)data);
+	return 0;
+}
 
 int main(int argc, char * argv[])
 {
-	printf("[%s] Hello from %s!\n", PACKAGE_NAME, argv[0]);
+	pthread_t tid;
+	pthread_create(&tid, 0, print_it, argv[0]);
+	pthread_join(tid, 0);
 	return 0;
 }
 
