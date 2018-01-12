1627a40ab7e99f98a7cddf0deecf728d28b66b97
Antonio Quintero <man.quintero@gmail.com>
2018-01-11 20:36:47 -0600

    Swithc to LTDL

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@
 #include <config.h>
 #endif
 
-#if HAVE_DLFCN_H
-#include <dlfcn.h>
+#if HAVE_LTDL_H
+#include <ltdl.h>
 #endif
 
 #define DEFAULT_SALUTATION "Hello"
@@ -15,26 +15,38 @@ int main(int argc, char * argv[])
 {
 	const char * salutation = DEFAULT_SALUTATION;
 
-#if HAVE_DLFCN_H
-	void * module;
+#if HAVE_LTDL_H
+	int ltdl;
+	lt_dlhandle module;
 	get_salutation_t * get_salutation_fp = 0;
 
-	module = dlopen("./module.so", RTLD_NOW);
-	if(module != 0)
+	LTDL_SET_PRELOADED_SYMBOLS();
+
+	ltdl = lt_dlinit();
+	if (ltdl == 0)
 	{
-		get_salutation_fp = (get_salutation_t *)
-			dlsym(module, GET_SALUTATION_SYM);
-		if (get_salutation_fp != 0)
-			salutation = get_salutation_fp();
+		module = ld_dlopen("modules/hithere/hithre.la");
+		if(module != 0)
+		{
+			get_salutation_fp = (get_salutation_t *)
+				dlsym(module, GET_SALUTATION_SYM);
+			if (get_salutation_fp != 0)
+				salutation = get_salutation_fp();
+		}
 	}
 #endif
 
 	jupiter_print(salutation, argv[0]);
 
-#if HAVE_DLFCN_H
-	if(module != 0)
-		dlclose(module);
+#if HAVE_LTDL_H
+	if (ltdl == 0)
+	{
+		if(module != 0)
+			lt_dlclose(module);
+	lt_dlexit();
+	}
 #endif
+
 	return 0;
 }
 
